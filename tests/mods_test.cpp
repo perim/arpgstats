33874cd8a96cd5daec7f790277bba5d789d2f3df
Per Mathisen <per.mathisen@gmail.com>
2026-01-05 23:09:36 +0100

    Improve and enable mods test

--- tests/mods_test.cpp
+++ tests/mods_test.cpp
@@ -14,6 +14,16 @@ int main(int argc, char** argv)
 	bool r = read_mods(argv[1]);
 	assert(r);
 	const mod_data& d = get_mod_data(0);
-	assert(!d.description.empty()); // the only require field
+	assert(!d.description.empty()); // the only required field
+
+	const std::vector<std::string>& types = get_mod_types();
+	assert(types.size() > 0);
+
+	const std::vector<std::string>& classifications = get_mod_classifications();
+	assert(classifications.size() > 0);
+
+	auto idx = get_classification_index("Craft");
+	assert(idx);
+
 	return 0;
 }
